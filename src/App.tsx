1fcb143d201aed4e458ad3abeec7fb959207a318
solymoskm <marton.schubert@gmail.com>
2025-04-17 11:09:38 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,9 @@
+import React from 'react';
 
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontSize: '1.5rem' }}>
+      Ráérés naptár működik! ✅
+    </div>
+  );
+}
